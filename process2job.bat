b6af9844d42e7107831cd706fa056a86cf2f2915
Pikurrot <pikurrot@gmail.com>
2024-11-28 15:16:21 +0100

    Define variables OUT_DIR and DATA_DIR

--- process2job.bat
+++ process2job.bat
@@ -1,9 +1,13 @@
 @echo off
 
+REM Define variables
+set "OUT_DIR=out\"
+set "DATA_DIR=D:\datasets\"
+
 REM Ensure an argument is passed
 if "%~1"=="" (
-    echo Error: No arguments provided. Usage: %~nx0 --[train|test] --[args...]
-    exit /b 1
+	echo Error: No arguments provided. Usage: %~nx0 --[train|test] --[args...]
+	exit /b 1
 )
 
 REM Get the first argument
@@ -12,10 +16,10 @@ shift /1
 
 REM Check the mode and call the appropriate Python script
 if "%MODE%"=="--train" (
-    python train.py %* --out_dir="out\" --data_dir="D:\datasets\"
+	python train.py %* --out_dir="%OUT_DIR%" --data_dir="%DATA_DIR%"
 ) else if "%MODE%"=="--test" (
-    python test.py %* --out_dir="out\" --data_dir="D:\datasets\"
+	python test.py %* --out_dir="%OUT_DIR%" --data_dir="%DATA_DIR%"
 ) else (
-    echo Error: Invalid mode '%MODE%'. Use '--train' or '--test'.
-    exit /b 1
+	echo Error: Invalid mode '%MODE%'. Use '--train' or '--test'.
+	exit /b 1
 )
--- process2job.sh
+++ process2job.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Define variables
+OUT_DIR="out/"
+DATA_DIR="/media/eric/D/datasets/"
+
 # Ensure an argument is passed
 if [ "$#" -lt 1 ]; then
-    echo "Error: No arguments provided. Usage: $0 --[train|test] --[args...]"
-    exit 1
+	echo "Error: No arguments provided. Usage: $0 --[train|test] --[args...]"
+	exit 1
 fi
 
 # Get the first argument
@@ -12,10 +16,10 @@ shift # Remove the first argument from the list
 
 # Check the mode and call the appropriate Python script
 if [ "$MODE" == "--train" ]; then
-    python train.py "$@" --out_dir="out/" --data_dir="/media/eric/D/datasets/"
+	python train.py "$@" --out_dir="$OUT_DIR" --data_dir="$DATA_DIR"
 elif [ "$MODE" == "--test" ]; then
-    python test.py "$@" --out_dir="out/" --data_dir="/media/eric/D/datasets/"
+	python test.py "$@" --out_dir="$OUT_DIR" --data_dir="$DATA_DIR"
 else
-    echo "Error: Invalid mode '$MODE'. Use '--train' or '--test'."
-    exit 1
+	echo "Error: Invalid mode '$MODE'. Use '--train' or '--test'."
+	exit 1
 fi
