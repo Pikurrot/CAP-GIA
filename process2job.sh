ca737ee2391c411f0679fe6fd55d567f55195db3
Eric Lopez <90217719+Pikurrot@users.noreply.github.com>
2024-11-28 15:03:30 +0100

    Add process2job.sh via upload

---
+++ process2job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ensure an argument is passed
+if [ "$#" -lt 1 ]; then
+    echo "Error: No arguments provided. Usage: $0 --[train|test] --[args...]"
+    exit 1
+fi
+
+# Get the first argument
+MODE="$1"
+shift # Remove the first argument from the list
+
+# Check the mode and call the appropriate Python script
+if [ "$MODE" == "--train" ]; then
+    python train.py "$@" --out_dir="out/" --data_dir="/media/eric/D/datasets/"
+elif [ "$MODE" == "--test" ]; then
+    python test.py "$@" --out_dir="out/" --data_dir="/media/eric/D/datasets/"
+else
+    echo "Error: Invalid mode '$MODE'. Use '--train' or '--test'."
+    exit 1
+fi
