bed324508a953fdaf31362c45b49191475982db6
Pikurrot <pikurrot@gmail.com>
2024-12-14 11:17:46 +0100

    Decrease lambda contrastive

--- src/ViTGptVED.py
+++ src/ViTGptVED.py
@@ -98,7 +98,7 @@ class ViTGptVED(nn.Module):
 			temperature: float=0.7,
 			repetition_penalty: float=2.0,
 			length_penalty: float=3.0,
-			lambda_contrastive: float=10.0, # Contrastive loss weight
+			lambda_contrastive: float=3.0, # Contrastive loss weight
 			inference_mode: Literal["sampling", "beam_search"] = "beam_search",
 			n_beams: int=5,
 			top_k: int=50,
